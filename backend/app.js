dbac2e5f09735e3715e7aceff2276c8b47f6dbf5
Tyrique-Stratusolve <tyrique.debruin@stratusolve.com>
2026-06-17 20:21:48 +0200

    Task 2 --- Refactor Fibonacci functionality and update UI elements for better clarity --- https://web.divblox.app/#/delivery/55926fcbbdf73794d3d8ee5d1adc73bd

--- backend/app.js
+++ backend/app.js
@@ -5,64 +5,23 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
+app.use(express.json());
 
-function addAll(array) {
-    if (array.length === 0) {
-        return 0;
-    }
-
-    let grandTotal = array.reduce((accumulator, current) => accumulator + current, 0);
-    array.shift();
-
-    return grandTotal + addAll(array);
+function nearestFibonacci(number) {
+    return number;
 }
 
-function addAllForLoop(array) {
-    let grandTotal = 0;
-    
-    for (let i = 0; i < array.length; i++) {
-        let sum = 0;
-        sum = array.slice(i).reduce((accumulator, current) => accumulator + current, 0);
-        grandTotal += sum;
-    }
-
-    return grandTotal;
+function generateFibonacci(number) {
+    return number;
 }
 
-function addAllShift(array) {
-    let grandTotal = 0;
-
-    while (array.length > 0) {
-        let currentSum = array.reduce((accumulator, current) => accumulator + current, 0);
-        array.shift();
-        grandTotal = grandTotal + currentSum;
-    }
-
-    return grandTotal;
-}
-
-function addAllPop(array) {
-    let grandTotal = 0;
-
-    while (array.length > 0) {
-        let currentSum = array.reduce((accumulator, current) => accumulator + current, 0);
-        array.pop();
-        grandTotal = grandTotal + currentSum;
-    }
-
-    return grandTotal;
-}
-
-app.get("/api/data", (request, response) => {
-    const array = [1, 1, 1, 1, 1];
+app.post("/api/data", (request, response) => {
+    const fibNum = request.body.fibNum;
 
     const dataFromNode = {
-        array: array,
         results: {
-            recursive: addAll([...array]),
-            forLoop: addAllForLoop([...array]),
-            shift: addAllShift([...array]),
-            pop: addAllPop([...array]),
+            nearestFibonacci: nearestFibonacci(fibNum),
+            generateFibonacci: generateFibonacci(fibNum),
         },
     };
 
