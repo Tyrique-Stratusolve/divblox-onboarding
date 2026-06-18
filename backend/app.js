b77323ffcc6570ea9b6901409bb9940a1734c63c
Tyrique-Stratusolve <tyrique.debruin@stratusolve.com>
2026-06-18 13:34:31 +0200

    Task 2 --- Refactor Fibonacci functions and update input type in HTML --- https://web.divblox.app/#/delivery/55926fcbbdf73794d3d8ee5d1adc73bd

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,32 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-function nearestFibonacci(number) {
-    return number;
+function nearestFibonacci(maxVal, currentNumVal = 0, nextNumVal = 1) {
+    if (maxVal <= 0) return [];
+
+    let fibArr = [];
+    
+    while (currentNumVal <= maxVal) {
+        fibArr.push(currentNumVal);
+        const tempNumVal = currentNumVal + nextNumVal;
+        currentNumVal = nextNumVal;
+        nextNumVal = tempNumVal;
+    }
+
+    return fibArr;
 }
 
-function generateFibonacci(number) {
-    return number;
+function generateFibonacci(numTerms, initalNumVal = 0, secondNumVal = 1, fibArr = []) {
+    if (numTerms <= 0) return fibArr;
+
+    fibArr.push(initalNumVal);
+    let nextNumVal = initalNumVal + secondNumVal;
+
+    return generateFibonacci(numTerms - 1, secondNumVal, nextNumVal, fibArr);
 }
 
 app.post("/api/data", (request, response) => {
-    const fibNum = request.body.fibNum;
+    let fibNum = parseInt(request.body.fibNum);
 
     const dataFromNode = {
         results: {
