5b9097a0cc0f15cd41a18b7028e335d000bc9dfc
Tyrique-Stratusolve <tyrique.debruin@stratusolve.com>
2026-06-23 14:28:50 +0200

    Task 7 --- Add database configuration, query function, and person table schema --- https://web.divblox.app/#/delivery/c2894b49b2ff28c3df86f3a63231a857

---
+++ backend/database/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "dev_password",
+        database: "divblox-onboarding",
+    },
+    port: 3000,
+};
+export default config;
---
+++ backend/database/db.js
@@ -0,0 +1,6 @@
+import mysql from "mysql2/promise";
+import config from "./config";
+
+async function query(sql, parameters) {
+    
+}
